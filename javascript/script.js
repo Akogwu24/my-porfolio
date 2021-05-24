8c702812d21553536f08e2dbc494678bafbba7fa
Akogwu24 <akogwu.emmanuel24@gmail.com>
2021-05-24 15:31:31 +0100

    text animation done

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,17 @@
+//typing amination
+let myName = document.getElementById('name');
+const text = myName.textContent;
+let index = 0;
+function typeName() {
+  myName.textContent = text.slice(0, index);
+  index++;
+  if (index > text.length - 1) {
+    index = 0;
+  }
+}
+
+setInterval(typeName, 500);
+
 $(document).ready(function () {
   $('#menu').click(function () {
     $(this).toggleClass('fa-times');
